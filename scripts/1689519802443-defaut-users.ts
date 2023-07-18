f501a300ab443824bedbf2ca1190debf49c6f7ea
Paul Tran <paul.tran.0920@gmail.com>
2023-07-18 14:28:32 +0700

    init some video detail

--- scripts/1689519802443-defaut-users.ts
+++ scripts/1689519802443-defaut-users.ts
@@ -42,6 +42,16 @@ export class DefautUsers1689519802443 implements MigrationInterface {
       INSERT INTO profile VALUES('dccb4325-e5b0-4c7a-8fe9-e0dc59364c28', '2023-07-16 09:49:00.057927', '2023-07-16 09:49:00.057927', 'User 1', '12355667899', NULL, '4e66529c-be91-48f7-a966-9a8bbace8645');
       `,
     );
+    await queryRunner.query(
+      `
+      INSERT INTO youtube VALUES('b3930052-43b2-4850-9928-435016fde1cf', '2023-07-18 07:05:30', '2023-07-18 07:05:30', '989-7xsRLR4', 'https://www.youtube.com/watch?v=989-7xsRLR4', 'Vitas - The 7th Element', 'Vitas - The 7th ElementMonatomic Music www.monatomicmusic.comVitas Official Merch Store!✔ https://teespring.com/stores/vitasOfficial Site ✔ http://vitas.com....', 'VITAS', 'https://i.ytimg.com/vi/989-7xsRLR4/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGBMgJih_MA8=&rs=AOn4CLB9DmaNyj6IxUhrcr9Spsh54U0i7w');
+      `,
+    );
+    await queryRunner.query(
+      `
+      INSERT INTO youtube VALUES('d58d281a-ed89-4bf0-8f59-79f9032e8cb8', '2023-07-18 07:05:52', '2023-07-18 07:05:52', 'naa9iY8pFnc', 'https://www.youtube.com/watch?v=naa9iY8pFnc', 'Bond - Victory  / Albert Hall', 'Recently found this music (band) via Andre Rieu - WOW so Uplifting....', 'Abraham Tol', 'https://i.ytimg.com/vi/naa9iY8pFnc/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AH-BIAC6AKKAgwIABABGH8gGigTMA8=&rs=AOn4CLAxNeW4Q3RTg1oYn5W4CuIqDK2LRA');
+      `,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {}
